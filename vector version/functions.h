e55a87fcfecea6c38fff0d810a0ec0540989c7ad
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-04-12 20:49:56 +0300

    update

--- vector version/functions.h
+++ vector version/functions.h
@@ -13,14 +13,40 @@
 using namespace std;
 using namespace std::chrono;
 
-struct studentas
-{
-    string vardas;
-    string pavarde;
-    vector<int> ndrez; //sudaromas vektorius
-    int erez;
-    double gbalas;
+class studentas {
+// realizacija
+private:
+  string vardas;
+  string pavarde;
+  vector<int> ndrez;
+  int erez;
+  double gbalas;
+  // interfeisas
+  public:
+      studentas(); // default konstruktorius
+      studentas(const string& v, const string& p, const vector<int>& nd, int e, double g);
+
+      string getVardas() const { return vardas; }                   // get'eriai
+      string getPavarde() const { return pavarde; }                 // get'eriai
+      vector<int> getNdrez() const { return ndrez; }
+      int getErez() const { return erez; }
+      double getGbalas() const { return gbalas; } // get'eriai
+      
+      void setVardas(const string& v) { vardas = v; }
+      void setPavarde(const string& p) { pavarde = p; }
+      void setNdrez(const vector<int>& nd) { ndrez = nd; }
+      void setErez(int e) { erez = e; }
+      void setGbalas(double g) { gbalas = g; }                     // set'eriai
 };
+
+// struct studentas
+// {
+//     string vardas;
+//     string pavarde;
+//     vector<int> ndrez; //sudaromas vektorius
+//     int erez;
+//     double gbalas;
+// };
 
 extern int tlaikas;
 
