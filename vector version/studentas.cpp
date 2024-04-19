03d41923bd1d1bcc8eeb7f6f3e5444541294d112
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-04-19 21:17:29 +0300

    update

--- vector version/studentas.cpp
+++ vector version/studentas.cpp
@@ -1,6 +1,15 @@
 #include "studentas.h"
+#include <utility>
 
 studentas::studentas() : erez(0), gbalas(0.0) {}
 
 studentas::studentas(const string& v, const string& p, const vector<int>& nd, int e, double g)
     : vardas(v), pavarde(p), ndrez(nd), erez(e), gbalas(g) {}
+
+// destruktorius
+studentas::~studentas() {
+ndrez.clear();}
+
+// copy konstruktorius
+studentas::studentas(const studentas& kit) 
+    : vardas(kit.vardas), pavarde(kit.pavarde), ndrez(kit.ndrez), erez(kit.erez), gbalas(kit.gbalas) {}
