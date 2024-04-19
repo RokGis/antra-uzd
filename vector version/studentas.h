692dce0164f8def26b44c0f53b50d3d49ba4cf03
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-04-19 21:42:27 +0300

    update

--- vector version/studentas.h
+++ vector version/studentas.h
@@ -9,34 +9,38 @@
 
 using namespace std;
 
-class studentas {
-// realizacija
+#include "zmogus.h"
+#include <vector>
+
+class Studentas : public Zmogus {
 private:
-  string vardas;
-  string pavarde;
-  vector<int> ndrez;
-  int erez;
-  double gbalas;
-  // interfeisas
-  public:
-      studentas(); // default konstruktorius
-      studentas(const string& v, const string& p, const vector<int>& nd, int e, double g);
+    std::vector<int> ndrez;
+    int erez;
+    double gbalas;
 
-      ~studentas(); // destruktorius
+public:
+    Studentas(const std::string& v, const std::string& p, const std::vector<int>& nd, int e, double g)
+        : Zmogus(v, p), ndrez(nd), erez(e), gbalas(g) {}
 
-      studentas(const studentas& kit); // copy konstruktorius
+    // Destructor
+    virtual ~Studentas();
 
-      studentas& operator=(const studentas& kit); // priskyrimo operatorius
+    // Copy constructor
+    Studentas(const Studentas& other);
 
-      studentas(studentas&& kit) noexcept; // move konstruktorius
+    // Copy assignment operator
+    Studentas& operator=(const Studentas& other);
 
-      studentas& operator=(studentas&& kit) noexcept;
+    // Move constructor
+    Studentas(Studentas&& other) noexcept;
 
-      string getVardas() const { return vardas; }                   // get'eriai
-      string getPavarde() const { return pavarde; }                 // get'eriai
-      vector<int> getNdrez() const { return ndrez; }
-      int getErez() const { return erez; }
-      double getGbalas() const { return gbalas; } // get'eriai
+    // Move assignment operator
+    Studentas& operator=(Studentas&& other) noexcept;
+
+    // Functions for getting ndrez, erez, and gbalas
+    std::vector<int> getNdrez() const { return ndrez; }
+    int getErez() const { return erez; }
+    double getGbalas() const { return gbalas; }
       
       void setVardas(const string& v) { vardas = v; }
       void setPavarde(const string& p) { pavarde = p; }
