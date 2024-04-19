32484f4fb90811de4ffb1315b8f563f13ef521a9
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-04-19 21:19:11 +0300

    update

--- vector version/studentas.cpp
+++ vector version/studentas.cpp
@@ -13,3 +13,31 @@ ndrez.clear();}
 // copy konstruktorius
 studentas::studentas(const studentas& kit) 
     : vardas(kit.vardas), pavarde(kit.pavarde), ndrez(kit.ndrez), erez(kit.erez), gbalas(kit.gbalas) {}
+
+// priskyrimo operatorius
+studentas& studentas::operator=(const studentas& kit) {
+    if (this != &kit) {
+        vardas = kit.vardas;
+        pavarde = kit.pavarde;
+        ndrez = kit.ndrez;
+        erez = kit.erez;
+        gbalas = kit.gbalas;
+    }
+    return *this;
+}
+
+// move konstruktorius
+studentas::studentas(studentas&& kit) noexcept
+    : vardas(std::move(kit.vardas)), pavarde(std::move(kit.pavarde)), ndrez(std::move(kit.ndrez)), erez(kit.erez), gbalas(kit.gbalas) {}
+
+// priskyrimo operatorius
+studentas& studentas::operator=(studentas&& kit) noexcept {
+    if (this != &kit) {
+        vardas = std::move(kit.vardas);
+        pavarde = std::move(kit.pavarde);
+        ndrez = std::move(kit.ndrez);
+        erez = kit.erez;
+        gbalas = kit.gbalas;
+    }
+    return *this;
+}
