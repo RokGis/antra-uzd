da127de3eeff5c367f094889315f8906563067f1
RokGis <rokas.gilys@mif.stud.vu.lt>
2024-05-24 22:08:23 +0300

    update

--- vector version/test.cpp
+++ vector version/test.cpp
@@ -2,44 +2,91 @@
 #include "catch2/catch.hpp"
 #include "vector.h"
 
-TEST_CASE("Vector operations", "[vector]") {
-    SECTION("Default constructor and element access") {
-        Vector<int> v;
-        REQUIRE(v.size() == 0);
-        REQUIRE(v.capacity() == 0);
+TEST_CASE("Default Constructor", "[Default Constructor]") {
+    Vector<int> v;
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
+    REQUIRE(v.empty());
+}
 
-        // Test out-of-range access
-        REQUIRE_THROWS_AS(v.at(0), std::out_of_range);
-
-        // Test empty vector access
-        REQUIRE_THROWS_AS(v.front(), std::out_of_range);
-        REQUIRE_THROWS_AS(v.back(), std::out_of_range);
+TEST_CASE("Initializer List Constructor", "[Initializer List Constructor]") {
+    Vector<int> v = {1, 2, 3, 4, 5};
+    REQUIRE(v.size() == 5);
+    REQUIRE(v.capacity() >= 5);
+    for (size_t i = 0; i < v.size(); ++i) {
+        REQUIRE(v[i] == i + 1);
     }
+}
 
-    SECTION("Pushing back elements and reallocations") {
-        Vector<int> v;
-        size_t initial_capacity = v.capacity();
-
-        // Push back elements
-        for (int i = 0; i < 1000; ++i) {
-            v.push_back(i);
-        }
-
-        // Test size and capacity
-        REQUIRE(v.size() == 1000);
-        REQUIRE(v.capacity() >= initial_capacity); // Capacity should be increased
-
-        // Check reallocations
-        size_t reallocations = v.getReallocationCount();
-        INFO("Number of reallocations: " << reallocations);
-        REQUIRE(reallocations > 0); // There should be at least one reallocation
-
-        // Test element access
-        REQUIRE(v.front() == 0);
-        REQUIRE(v.back() == 999);
-        REQUIRE(v[500] == 500);
-
-        // Test out-of-range access
-        REQUIRE_THROWS_AS(v.at(1000), std::out_of_range);
+TEST_CASE("Copy Constructor", "[Copy Constructor]") {
+    Vector<int> v1 = {1, 2, 3};
+    Vector<int> v2(v1);
+    REQUIRE(v2.size() == v1.size());
+    for (size_t i = 0; i < v1.size(); ++i) {
+        REQUIRE(v2[i] == v1[i]);
     }
+}
+
+TEST_CASE("Move Constructor", "[Move Constructor]") {
+    Vector<int> v1 = {1, 2, 3};
+    Vector<int> v2(std::move(v1));
+    REQUIRE(v1.size() == 0);
+    REQUIRE(v2.size() == 3);
+}
+
+TEST_CASE("Copy Assignment Operator", "[Copy Assignment Operator]") {
+    Vector<int> v1 = {1, 2, 3};
+    Vector<int> v2;
+    v2 = v1;
+    REQUIRE(v2.size() == v1.size());
+    for (size_t i = 0; i < v1.size(); ++i) {
+        REQUIRE(v2[i] == v1[i]);
+    }
+}
+
+TEST_CASE("Move Assignment Operator", "[Move Assignment Operator]") {
+    Vector<int> v1 = {1, 2, 3};
+    Vector<int> v2;
+    v2 = std::move(v1);
+    REQUIRE(v1.size() == 0);
+    REQUIRE(v2.size() == 3);
+}
+
+TEST_CASE("Element Access", "[Element Access]") {
+    Vector<int> v = {1, 2, 3};
+    REQUIRE(v[0] == 1);
+    REQUIRE(v.at(1) == 2);
+    REQUIRE(v.front() == 1);
+    REQUIRE(v.back() == 3);
+}
+
+TEST_CASE("Modifiers", "[Modifiers]") {
+    Vector<int> v;
+    v.push_back(1);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v.capacity() >= 1);
+    REQUIRE(v[0] == 1);
+
+    v.pop_back();
+    REQUIRE(v.size() == 0);
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.erase(v.begin() + 1);
+    REQUIRE(v.size() == 2);
+    REQUIRE(v[0] == 1);
+    REQUIRE(v[1] == 3);
+
+    v.clear();
+    REQUIRE(v.size() == 0);
+}
+
+TEST_CASE("Iterators", "[Iterators]") {
+    Vector<int> v = {1, 2, 3};
+    int sum = 0;
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        sum += *it;
+    }
+    REQUIRE(sum == 6);
 }
